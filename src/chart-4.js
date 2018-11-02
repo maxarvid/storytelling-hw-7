226f4ea69665bd61daeab5adbf9b660636cd253e
Max <max.arvid.anderson@gmail.com>
2018-11-02 14:49:59 -0400

    fixed merge vulnerability

--- src/chart-4.js
+++ src/chart-4.js
@@ -6,20 +6,16 @@ var height = 500 - margin.top - margin.bottom
 var width = 400 - margin.left - margin.right
 
 // And grabbing your container
-// var container = d3.select('#chart-4')
+var container = d3.select('#chart-4')
 
 // Create your scales
+var xPositionScale = d3
+  .scaleOrdinal()
+  .domain([-6, 6])
+  .range([0, width])
+
+var yPositionScale = d3.scaleLinear().range([height, 0])
 
 // Create your area generator
 
 // Read in your data, then call ready
-// d3.csv(require('./climate-data.tsv')
-//   .then(ready)
-//   .catch(err => {
-//     console.log(err)
-//   })
-
-// Write your ready function
-// function ready(datapoints) {
-//   console.log('data is', datapoints)
-// }
